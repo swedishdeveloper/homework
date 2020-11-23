355dd4793c2eaa57ac41a132acd4ced4a0162128
Benjamin Ojanne <benjaminprogrammering@gmail.com>
2020-11-23 20:58:56 +0100

    Solve 2

--- 48/2/script.js
+++ 48/2/script.js
@@ -2,7 +2,6 @@
 // Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
 // Om talet blir 6, avsluta loopen
 
-
 // Exempel på output:
 // 4
 // 7
@@ -12,3 +11,9 @@
 // 1
 // 1
 // 6
+
+while (true) {
+    let random = Math.floor(Math.random() * 11)
+    console.log(random)
+    if (random === 6) break;
+}
